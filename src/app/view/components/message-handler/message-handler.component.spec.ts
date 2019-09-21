f9c9ece93f90f183d27a7abe4d78f8e09dcb58b0
Luke <lukerbab@gmail.com>
2019-09-21 20:22:06 +0100

    featuire: updated tests for message handler

--- src/app/view/components/message-handler/message-handler.component.spec.ts
+++ src/app/view/components/message-handler/message-handler.component.spec.ts
@@ -19,9 +19,21 @@ describe('MessageHandlerComponent', () => {
     fixture = TestBed.createComponent(MessageHandlerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.displayedErrorMessage = {
+      type: 'test',
+      message: 'Hello World'
+    };
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display an alert with correct message', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    expect(component).toBeTruthy();
+    const alertElement = bannerElement.querySelector('.alert');
+    expect(alertElement.textContent).toEqual('Hello World');
   });
 });
