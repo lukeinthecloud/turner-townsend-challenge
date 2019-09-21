c2b0df6243f3858c071a3d39d78b6053defd7795
Luke <lukerbab@gmail.com>
2019-09-21 22:31:18 +0100

    feature: added tests for search component

--- src/app/view/components/search/search.component.spec.ts
+++ src/app/view/components/search/search.component.spec.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { SearchComponent } from './search.component';
 
@@ -10,23 +11,44 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ],
+      declarations: [SearchComponent],
       imports: [
         CommonModule,
         FormsModule,
         ReactiveFormsModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+    spyOn(component.searchItemEmitter, 'emit');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSubmit when clicked with value', () => {
+    component.searchFormGroup.controls.searchInput.setValue('test');
+    const formElement = fixture.debugElement
+      .query(By.css('form'));
+
+    formElement.triggerEventHandler('ngSubmit', null);
+
+    expect(component.searchItemEmitter.emit).toHaveBeenCalledWith('test');
+  });
+
+  it('should call the emitter with nothing', () => {
+    component.searchFormGroup.controls.searchInput.setValue('test');
+    const formElement = fixture.debugElement
+      .query(By.css('.btn-outline-warning'));
+
+    formElement.triggerEventHandler('click', null);
+
+    expect(component.searchItemEmitter.emit).toHaveBeenCalledWith('');
   });
 });
